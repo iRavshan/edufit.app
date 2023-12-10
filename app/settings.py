78db217071b0bf1e9e53f5df7effc9b1415342cf
Sadikov Dev <ravshan4ikov@gmail.com>
2023-12-10 17:14:25 +0500

    sdfsd

--- app/settings.py
+++ app/settings.py
@@ -98,7 +98,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')] 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
