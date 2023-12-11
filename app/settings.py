9dae726fb86cf58cfdc872e96622fe05bfc440da
Sadikov Dev <ravshan4ikov@gmail.com>
2023-12-11 11:19:02 +0500

    prod

--- app/settings.py
+++ app/settings.py
@@ -61,7 +61,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'app.wsgi.application'
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
--- app/views.py
+++ app/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from user.models import CustomUser, Grade
 from competition.models import Competition
 
+@login_required(login_url='/user/login')
 def Home(request):
     user = CustomUser.objects.get(id=request.user.id)
     users = CustomUser.objects.filter(grade=user.grade)
     grades = Grade.objects.all()[:10]
     competitions = Competition.objects.filter(terminated=False)
+    
     context={
         'user': user,
         'top_users': users,
         'grades': grades,
         'competitions': competitions
     }
-    return render(request, 'home/authorizedHome.html', context)
+    return render(request, 'home/home.html', context)
