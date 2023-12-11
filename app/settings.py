0600e8609a3e730123ce678d55b589ba38e67910
Sadikov Dev <ravshan4ikov@gmail.com>
2023-12-11 22:38:00 +0500

    jh

--- app/settings.py
+++ app/settings.py
@@ -104,10 +104,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
