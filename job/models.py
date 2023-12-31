57c4846abb7b5eb420109f81ae7058534a1dc4a0
Sadikov Dev <ravshan4ikov@gmail.com>
2023-12-31 18:41:54 +0500

    the app "jobs" was added

--- job/models.py
+++ job/models.py
@@ -2,19 +2,25 @@ import uuid
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+class JobCategory(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, unique=True, null=False, default=uuid.uuid4)
+    name = models.CharField(_('name'), null=False, max_length=50, unique=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
 
 class JobSubcategory(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, unique=True, null=False, default=uuid.uuid4)
-    name = models.CharField(_('name'), max_length=50, unique=True)
+    name = models.CharField(_('name'), null=False, max_length=50, unique=True)
+    category = models.ForeignKey(JobCategory, null=False, on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ['name']
+        
     def __str__(self):
         return self.name
         
-
-class JobCategory(models.Model):
-    id = models.UUIDField(primary_key=True, editable=False, unique=True, null=False, default=uuid.uuid4)
-    name = models.CharField(_('name'), max_length=50, unique=True)
-    subcategories = models.ForeignKey(JobSubcategory, null=True, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
--- job/views.py
+++ job/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .models import JobCategory, JobSubcategory
 
 def Jobs(request):
-    return render(request, 'job/jobs.html')
+    categories = []
+    jobs = JobCategory.objects.all()
+
+    for job in jobs:
+        subcategories = JobSubcategory.objects.filter(id=job.id)
+        categories.append({
+            'id': job.id,
+            'name': job.name,
+            'subcategories': subcategories
+        })
+    context = {
+        "jobs": categories
+    }
+    return render(request, 'job/jobs.html', context)
 
