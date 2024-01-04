11fc596925cca909dc5c72185a1bf4f4683a7b3b
Sadikov Dev <ravshan4ikov@gmail.com>
2024-01-04 11:05:05 +0500

    recollected static files after installed owl-carousel

---
+++ staticfiles/js/owl-carousel.js
@@ -0,0 +1,21 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:20,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    autoWidth:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
